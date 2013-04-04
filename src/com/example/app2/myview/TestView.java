ffd7d0585c9f8583692b80ea57cc1e60f2a66ab9
lin134340 <lylcsmhn@gmail.com>
2013-04-04 15:31:42 +0800

    none

---
+++ src/com/example/app2/myview/TestView.java
@@ -0,0 +1,12 @@
+package com.example.app2.myview;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.View;
+
+class PieChart extends View {
+    public PieChart(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+}
+
